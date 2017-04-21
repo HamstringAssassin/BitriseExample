0f9aff9fbe32a67b2fad472ef1d73b5d8352053b
Alan O'Connor <alanoc83@gmail.com>
2017-04-21 15:17:41 +0100

    test Update

--- BitriseExample/ViewController.swift
+++ BitriseExample/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let plistFileName = Bundle.main.infoDictionary?["TargetName"] as! String
         appLabel.text = plistFileName
     }
